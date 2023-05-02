e5de836049ecd3cf6c58789f447ffa954c8c424f
RonCollins-MM <muriukiron@gmail.com>
2023-05-02 23:17:26 +0300

    feat: add pre-request filtering of requests

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,32 +6,27 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-if os.getenv("AUTH_TYPE") == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-elif os.getenv("AUTH_TYPE") == "auth":
+if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
 
 @app.before_request
-def before_request_func():
-    """ before request function """
+def request_filter():
+    """function that runs before each request"""
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
+
     if auth is None:
-        return None
-    if auth.require_auth(request.path, excluded_paths):
-        pass
-    else:
-        return None
+        return
+    if not auth.require_auth(request.path, excluded_paths):
+        return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
@@ -47,9 +42,8 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
+    """Handler for unauthorized requests"""
+    return jsonify({'error': 'unauthorized'}), 401
 
 
 @app.errorhandler(403)
