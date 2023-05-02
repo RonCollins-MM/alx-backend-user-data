a504fc0e0416ea07c361adc2e4875c0e0119b124
RonCollins-MM <muriukiron@gmail.com>
2023-05-03 00:36:02 +0300

    feat: add full support for basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -67,3 +67,13 @@ class BasicAuth(Auth):
             return objs[0]
         else:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Retrieves the user associated with the request"""
+        if not request:
+            return None
+        auth_header = Auth().authorization_header(request)
+        auth_header = self.extract_base64_authorization_header(auth_header)
+        dec_header = self.decode_base64_authorization_header(auth_header)
+        uname, password = self.extract_user_credentials(dec_header)
+        return self.user_object_from_credentials(uname, password)
