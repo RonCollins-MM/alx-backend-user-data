8605819b75ab3a5f8ae340766a54755afb63a32a
RonCollins-MM <muriukiron@gmail.com>
2023-05-02 21:43:02 +0300

    add implementation for `require_auth` function

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,8 +15,30 @@ class Auth():
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks whether authentication is needed"""
-        return False
+        """Checks whether authentication is needed
+
+        parameters
+        ----------
+        path : str
+            Path to be checked
+        excluded_paths: List[str]
+            List of paths to be exlcuded
+
+        Returns
+        -------
+        Boolean
+            Whether the path should be authenticated or not
+        """
+
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == '':
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None):
         """Generates authorization header for a request"""
