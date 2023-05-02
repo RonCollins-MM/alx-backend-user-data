0c85c23ebe6a0c5d41804e00e6922dc89b7f09d9
RonCollins-MM <muriukiron@gmail.com>
2023-05-03 00:08:02 +0300

    feat: add decoding for base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Implementation of basic authentication
 """
 
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,16 @@ class BasicAuth(Auth):
         if 'Basic ' not in authorization_header:
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base_authentication_header: str):
+        """Decodes a base64 header"""
+        if base_authentication_header is None:
+            return None
+        if not isinstance(base_authentication_header, str):
+            return None
+        try:
+            decoded_h = b64decode(base_authentication_header)
+            return decoded_h.decode('utf-8')
+        except Exception as e:
+            return None
