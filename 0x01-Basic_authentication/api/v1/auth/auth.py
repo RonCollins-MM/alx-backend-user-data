468152f5f04ca8442b35d821933a34438d4df234
RonCollins-MM <muriukiron@gmail.com>
2023-05-02 20:43:05 +0300

    feat: define authentication template

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python3
-""" module to manage the API authentication """
+
+"""
+Defines the class `Auth` that is the template for all authentication
+systems.
+"""
+
 from flask import request
 from typing import List, TypeVar
 
 
-class Auth:
-    """ Class to manage the API authentication """
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Public method """
-        if path is None or excluded_paths is None or excluded_paths == []:
-            return True
-        if path in excluded_paths:
-            return False
-        if path[-1] == '/' and path[:-1] in excluded_paths:
-            return False
-        if path[-1] != '/' and path + '/' in excluded_paths:
-            return False
-        for exp in excluded_paths:
-            if exp[-1] == "*" and path.startswith(exp[:-1]):
-                return False
-        return True
+class Auth():
+    """
+    Template for all authentication systems
+    """
 
-    def authorization_header(self, request=None) -> str:
-        """ Public method """
-        if request is None or "Authorization" not in request.headers:
-            return None
-        return request.headers["Authorization"]
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks whether authentication is needed"""
+        return False
+
+    def authorization_header(self, request=None):
+        """Generates authorization header for a request"""
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Public method """
+        """Returns the user object associated with the request ??NOT SURE.
+        """
         return None
