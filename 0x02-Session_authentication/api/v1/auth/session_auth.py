1ccad8ece256180c3918287cab3584e37ebc811a
RonCollins-MM <muriukiron@gmail.com>
2023-05-03 18:48:26 +0300

    feat: add session ID generator

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,7 +11,9 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """ creates a Session ID for a user_id """
-        if not user_id or not isinstance(user_id, str):
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
