7f116f8aca13184cd62be1404ecfce93dd8cc1a9
RonCollins-MM <muriukiron@gmail.com>
2023-05-03 18:52:56 +0300

    feat: add function to return user_id assoc with session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,7 +21,9 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ returns a User ID based on a Session ID """
-        if not session_id or not isinstance(session_id, str):
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
 
